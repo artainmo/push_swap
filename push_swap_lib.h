00ebd2c692b7c620741fae8c9973bc68403736c3
Arthur Tainmont <artainmo@Arthurs-MacBook-Pro.local>
2024-10-27 20:06:03 +0100

    change lib name

--- push_swap_lib.h
+++ push_swap_lib.h
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# include "libs/libft_plus/libft.h"
+# include "libs/libft/libft.h"
 # include "libs/push_swap/push_swap.h"
 # include "libs/get_next_line/get_next_line.h"
 
